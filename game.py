f27190a7c062d4c551f41ec3a89d15d627ff3b17
Lovelace-Fan <edwardp9@vt.edu>
2020-03-07 02:49:59 -0500

    Force users to input a number. Also force users to input a valid coordinate pair in the board range. Lastly, announce the game is a cat/draw if no one won

--- game.py
+++ game.py
@@ -25,11 +25,24 @@ def check_for_winner(board):
     else:
         return False
 
-def spotFree(x, y, board):
-    if(board[x][y] != "_"):
+#This function checks if is free and unplayed on so far
+def spot_free(xcor, ycor, board):
+    if(board[xcor][ycor] != "_"):
         return False
     else:
         return True
+
+#This function checks if the x and y coordinates are out of the boundaries of the board
+def out_of_bounds(xcor, ycor):
+    if(0 <= xcor <= 2) and (0 <= ycor <=2):
+        return False
+    else:
+        return True
+
+#This function checks if the x and y from the user input were numbers
+def input_is_number(xcor, ycor):
+    return xcor.isdigit() and ycor.isdigit()
+
 
 
 #counter to keep track of turns
@@ -39,14 +52,32 @@ print_board(ttboard)
 while(count < 9):
     print("Your turn", player)
     print()
-    not_taken = True
-    x = int (input("Please input your x coordinate: "))
-    y = int (input("Please input your y coordinate: "))
+    x = input("Please input your x coordinate: ")
+    y = input("Please input your y coordinate: ")
     print()
-    not_taken = spotFree(x, y, ttboard)
+    
+    correct_input = input_is_number(x,y)
+    while(not correct_input):
+        x = input("Please input an x coordinate that is a number: ")
+        y = input("Please input a y coordinate that is a number: ")
+        correct_input = input_is_number(x,y)
+        print()
+    
+    x = int(x)
+    y = int(y)
+    print()
+    bound_status = out_of_bounds(x,y)
+    while(bound_status):
+        x = int (input("Please input an x coordinate that is in the valid range: "))
+        y = int (input("Please input a y coordinate that is in the valid range: "))
+        bound_status = out_of_bounds(x,y)
+        print()
+        
+    not_taken = spot_free(x, y, ttboard)
     while(not not_taken):
-        x = int (input("Please input an x coordinate that hasn't already been played: "))
-        y = int (input("Please input your y coordinate that hasn't already been played: "))
+        x = int (input("Please input a new x coordinate that because that x-y pair has already been played: "))
+        y = int (input("Please input a new y coordinate that because that x-y pair has already been played: "))
+        not_taken = spot_free(x, y, ttboard)
         print()
 
     
@@ -69,7 +100,9 @@ while(count < 9):
         
     print()
     count += 1
-    
+
+if(not check_for_winner(ttboard)):
+    print("The game is a CAT!")
 
     
 
